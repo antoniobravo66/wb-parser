0becb8f13f21ea0b5b44326041dd16236089590e
antoniobravo66 <bravo66antonio@gmail.com>
2025-07-10 20:05:10 +0300

    Delete parser.py
    замена

--- parser.py
+++
@@ -1,9 +0,0 @@
-def parse_wb_card(url):
-    return {
-        "url": url,
-        "title": "Тестовый товар",
-        "price": "999",
-        "rating": "4.9",
-        "description": "Описание товара",
-        "characteristics": "Материал / хлопок::Цвет / синий"
-    }
